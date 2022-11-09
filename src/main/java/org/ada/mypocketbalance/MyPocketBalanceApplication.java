7d64d4d0a51f264f9d4110d917357b24517d7b12
KennyMls <delgadokenny4@gmail.com>
2022-11-09 16:15:14 -0500

    Test

---
+++ src/main/java/org/ada/mypocketbalance/MyPocketBalanceApplication.java
@@ -0,0 +1,13 @@
+package org.ada.mypocketbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPocketBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPocketBalanceApplication.class, args);
+	}
+
+}
