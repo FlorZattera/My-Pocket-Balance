006b9986d3703bad7d595282890bd5516462697a
KennyMls <delgadokenny4@gmail.com>
2022-11-19 01:23:07 -0500

    borre repo de mas

--- mypocketbalance/repository/VendedorRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ada.mypocketbalance.repository;
-
-import org.ada.mypocketbalance.entity.Vendedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VendedorRepository extends JpaRepository<Vendedor,Integer> {
-}
