9885f41e0b2f16f4f21e702c6f0a7e01560cef10
KennyMls <delgadokenny4@gmail.com>
2022-11-19 01:02:38 -0500

    testing

--- src/main/java/org/ada/mypocketbalance/repository/DetalleFacturaRepository.java
+++ src/main/java/org/ada/mypocketbalance/repository/DetalleFacturaRepository.java
@@ -4,7 +4,7 @@ package org.ada.mypocketbalance.repository;
 import org.ada.mypocketbalance.entity.DetalleFactura;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DetalleFacturaRepository extends JpaRepository<DetalleFactura, Integer> {
+public interface DetalleFacturaRepository extends JpaRepository<DetalleFactura,Integer> {
 
 
 }
